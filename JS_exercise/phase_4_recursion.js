99e6a67c94cbbde4abecc488ff1e5d1f71a9c7bc
App Academy Student <appacademy@Apps-Mac-mini-4.local>
2018-07-19 17:18:57 -0700

    Work through half of phase 4

--- JS_exercise/phase_4_recursion.js
+++ JS_exercise/phase_4_recursion.js
@@ -10,23 +10,39 @@ function range(start, end){
 
 //EXERCISE 2=======================================
 function sumRec(arr) {
-  if(arr.length === 1){ 
+  if(arr.length <= 1){ 
     return arr[0];
   }
-  return sumRec(arr.slice(1,arr.length-1) + arr[0]);
+  let x = sumRec(arr.slice(1,arr.length)) + arr[0];
+  return x;
 }
 
-console.log(sumRec([1,2,3,4]));
+// console.log(sumRec([1,2,3,4]));
 
 //EXERCISE 3=======================================
 function exponent(base, exp) {
-  
+  if(exp === 1) { 
+    return base;
+  }
+  return base * exponent(base, exp-1);
 }
+
+// console.log(exponent(5,3));
 
 //EXERCISE 4=======================================
 function fibonacci(n) {
-  
+  if(n === 1){
+    return [1]; 
+  }
+  else if (n === 2) {
+    return [1,1];
+  }
+  else if(n === 3) {
+    return fibonacci(n-1).concat(fibonacci(n-2)[fibonacci(n-2).length-1] + fibonacci(n-3)[fibonacci(n-3).length-1]);
+  }
 }
+
+console.log(fibonacci(10));
 
 //EXERCISE 5=======================================
 function deepDup(arr) {
